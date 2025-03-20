f5be3928445cf1f0a2d78d81d470227cf064d12f
Julian Wang <julian@hotcake.app>
2025-03-20 16:30:30 +0800

    Fix: adjust search result click id in bottom search nav

--- pages/locators/android/navigation/search_locators.py
+++ pages/locators/android/navigation/search_locators.py
@@ -6,5 +6,5 @@ class SearchLocators:
       SEARCH_INPUT = (AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().text("搜尋")')
       SEARCH_RESULT = (AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().text("+886 972 205690").instance(0)')
       MEMBER_TAB = (AppiumBy.XPATH, '//android.view.View[@content-desc="會員"]/android.view.ViewGroup')
-      MEMBER_RESULT = (AppiumBy.ACCESSIBILITY_ID, '+886 972 205690, 王貝克')
+      MEMBER_RESULT = (AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().descriptionContains("+886 972 205690")')
       NO_DATA_TEXT = (AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().text("目前沒有資料")')  
