64a5e757f78176acfc842418f31a3da3db84b647
Julian Wang <julian@hotcake.app>
2025-04-24 11:26:03 +0800

    fix: adjust setup.py config, remove performance optimization settings

--- setup.py
+++ setup.py
@@ -46,11 +46,7 @@ if is_ci:
         options.set_capability('newCommandTimeout', 300)
         options.set_capability('androidInstallTimeout', 90000)
         options.set_capability('adbExecTimeout', 60000)
-        # 性能優化設置
-        options.set_capability('skipServerInstallation', True)
-        options.set_capability('skipDeviceInitialization', True)
-        options.set_capability('ignoreUnimportantViews', True)
-        options.set_capability('disableAndroidWatchers', True)
+    
     else:  # iOS
         options = XCUITestOptions()
         options.platform_name = 'iOS'
